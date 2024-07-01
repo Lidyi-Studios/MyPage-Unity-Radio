c4b7dc94d9109d3edb2391fa68c532836d1b726e
Juan Deyby Carlos-Chullo <deyby57@gmail.com>
2024-06-30 21:33:57 -0500

    feat: add play button

--- Assets/_App/Scripts/Radio.cs
+++ Assets/_App/Scripts/Radio.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using NAudio.Wave;
+using UnityEngine.UI;
 
 /// <summary>
 /// More info about NAudio: https://github.com/AstralSkies/RadioUnityStream/blob/main/Assets/Scripts/RadioManager.cs
@@ -11,7 +12,6 @@ using NAudio.Wave;
 public class Radio : MonoBehaviour
 {
     private readonly string _url = "https://lidyi.com/radio/server.mp3";
-    private AudioSource _audioSource;
     /// <summary>
     /// The MediaFoundationReader for audio processing.
     /// </summary>
@@ -21,10 +21,30 @@ public class Radio : MonoBehaviour
     /// The WaveOutEvent for audio output.
     /// </summary>
     private WaveOutEvent _waveOut;
+    
+    [SerializeField] private Button _playButton;
+    [SerializeField] private Image _playImage;
+    [SerializeField] private Sprite _playSprite;
+    [SerializeField] private Sprite _pauseSprite;
 
     private void Awake()
     {
-        _audioSource = GetComponent<AudioSource>();
+        _playButton.onClick.AddListener(() =>
+        {
+            if (_waveOut != null)
+            {   
+                if (_waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    _waveOut.Pause();
+                    _playImage.sprite = _playSprite;
+                }
+                else
+                {
+                    _waveOut.Play();
+                    _playImage.sprite = _pauseSprite;
+                }
+            }
+        });
     }
 
     void Start()
@@ -46,6 +66,35 @@ public class Radio : MonoBehaviour
         {
             Debug.LogError($"Error playing radio: {ex.Message}");
         }
+    }
+    
+    public void PlayRadio()
+    {
+        if (_waveOut != null)
+        {
+            _waveOut.Play();
+        }
+    }
+    
+    public void StopRadio()
+    {
+        if (_waveOut != null)
+        {
+            _waveOut.Stop();
+        }
+    }
+
+    public void SetVolume(float volume)
+    {
+        if (_waveOut != null)
+        {
+            _waveOut.Volume = Mathf.Clamp01(volume);
+        }
+    }
+
+    public float GetVolume()
+    {
+        return _waveOut != null ? _waveOut.Volume : 0;
     }
 
     private void OnDestroy()
